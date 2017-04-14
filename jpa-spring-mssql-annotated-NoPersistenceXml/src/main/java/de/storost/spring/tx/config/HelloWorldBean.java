5154a1769937d522f9ea1dbc1e0deb7d3ff465bf
ms748 <storost@web.de>
2017-04-14 22:12:04 +0200

    update

--- jpa-spring-mssql-annotated-NoPersistenceXml/src/main/java/de/storost/spring/tx/config/HelloWorldBean.java
+++ jpa-spring-mssql-annotated-NoPersistenceXml/src/main/java/de/storost/spring/tx/config/HelloWorldBean.java
@@ -6,10 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 import de.storost.spring.tx.HelloWorld;
 
-@Configuration
+@Component
 @PropertySource("classpath:app.properties")
 public class HelloWorldBean {
 
